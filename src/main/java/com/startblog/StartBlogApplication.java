7411ab1c5ccb75c64f8ccb42f134d3e7de2a1b2e
zhaorc <269032963@qq.com>
2022-06-08 11:05:30 +0800

    配置修改

--- src/main/java/com/startblog/StartBlogApplication.java
+++ src/main/java/com/startblog/StartBlogApplication.java
@@ -1,13 +1,15 @@
 package com.startblog;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StartBlogApplication {
-    
     public static void main(String[] args) {
-        SpringApplication.run(StartBlogApplication.class, args);
+        SpringApplication application = new SpringApplication(StartBlogApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
     
 }
